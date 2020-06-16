dd91ac55a5d4c729d71ec1584ffb5859c0b63b8f
Quinton Fults <coding.banjo@gmail.com>
2020-06-16 14:29:34 -0700

    code

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import './App.css'
 
 const App = () => {
   return (
-    <div>
-      <h1>Hello World!</h1>
-    </div>
+    <>
+      <h1 className="hotdog">Hello World!</h1>
+    </>
   )
 }
 
