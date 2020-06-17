4a3441855b8ad2a33c2b862132d1cce69dd364f8
Quinton Fults <coding.banjo@gmail.com>
2020-06-17 11:59:05 -0700

    code

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import Navbar from './components/Navbar'
 import Jumbotron from './components/Jumbotron'
+import Form from './components/Form'
+import List from './components/List'
 import './App.css'
 
 
@@ -8,63 +10,18 @@ const App = () => {
   return (
     <>
       <Navbar />
-      <Jumbotron />
-      <form>
-        <div className="form-row">
-          <div className="form-group col-md-6">
-            <label for="inputEmail4">Email</label>
-            <input type="email" className="form-control" id="inputEmail4" />
+      <div className="container">
+        <div className="row">
+          <Jumbotron />
+        </div>
+        <div className="row">
+          <div className="col-md-6">
+            <Form />
           </div>
-          <div className="form-group col-md-6">
-            <label for="inputPassword4">Password</label>
-            <input type="password" className="form-control" id="inputPassword4" />
+          <div className="col-md-6">
+            <List />
           </div>
         </div>
-        <div className="form-group">
-          <label for="inputAddress">Address</label>
-          <input type="text" className="form-control" id="inputAddress" placeholder="1234 Main St" />
-        </div>
-        <div className="form-group">
-          <label for="inputAddress2">Address 2</label>
-          <input type="text" className="form-control" id="inputAddress2" placeholder="Apartment, studio, or floor" />
-        </div>
-        <div className="form-row">
-          <div className="form-group col-md-6">
-            <label for="inputCity">City</label>
-            <input type="text" className="form-control" id="inputCity" />
-          </div>
-          <div className="form-group col-md-4">
-            <label for="inputState">State</label>
-            <select id="inputState" className="form-control">
-              <option selected>Choose...</option>
-              <option>...</option>
-            </select>
-          </div>
-          <div className="form-group col-md-2">
-            <label for="inputZip">Zip</label>
-            <input type="text" className="form-control" id="inputZip" />
-          </div>
-        </div>
-        <div className="form-group">
-          <div className="form-check">
-            <input className="form-check-input" type="checkbox" id="gridCheck" />
-            <label className="form-check-label" for="gridCheck">
-              Check me out
-      </label>
-          </div>
-        </div>
-        <button type="submit" className="btn btn-primary">Sign in</button>
-      </form>
-      <div className="list-group">
-        <a href="google.com" className="list-group-item list-group-item-action">Dapibus ac facilisis in</a>
-        <a href="google.com" className="list-group-item list-group-item-action list-group-item-primary">A simple primary list group item</a>
-        <a href="google.com" className="list-group-item list-group-item-action list-group-item-secondary">A simple secondary list group item</a>
-        <a href="google.com" className="list-group-item list-group-item-action list-group-item-success">A simple success list group item</a>
-        <a href="google.com" className="list-group-item list-group-item-action list-group-item-danger">A simple danger list group item</a>
-        <a href="google.com" className="list-group-item list-group-item-action list-group-item-warning">A simple warning list group item</a>
-        <a href="google.com" className="list-group-item list-group-item-action list-group-item-info">A simple info list group item</a>
-        <a href="google.com" className="list-group-item list-group-item-action list-group-item-light">A simple light list group item</a>
-        <a href="google.com" className="list-group-item list-group-item-action list-group-item-dark">A simple dark list group item</a>
       </div>
     </>
   )
