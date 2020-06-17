13f2066df2a52c3a258256d8ee5cea7b642242b1
Quinton Fults <coding.banjo@gmail.com>
2020-06-17 14:02:13 -0700

    code

--- src/App.js
+++ src/App.js
@@ -3,12 +3,16 @@ import Navbar from './components/Navbar'
 import Jumbotron from './components/Jumbotron'
 import Form from './components/Form'
 import List from './components/List'
+import Button from './components/Button'
 import './App.css'
 
 
 const App = () => {
   return (
     <>
+      <Button text="Props Button" />
+      <Button text="Second Button" />
+      <Button text="Third Button" />
       <Navbar />
       <div className="container">
         <div className="row">
