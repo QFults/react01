aae870def4e107405107feb7cf6b81a3f766264c
Quinton Fults <coding.banjo@gmail.com>
2020-06-17 10:42:34 -0700

    code

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import React from 'react'
 import './App.css'
 
+let nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
 const App = () => {
   return (
     <>
       <h1 className="hotdog">Hello World!</h1>
+
+      <h3>Numbers:</h3>
+      <ul>
+        {
+          nums.map(num => <li>{num}</li>)
+        }
+      </ul>
     </>
   )
 }
